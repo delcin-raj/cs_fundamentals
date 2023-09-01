6006bbaf9456749dab6167ee8d2541df8dddf523
saranch123 <143322610+saranch123@users.noreply.github.com>
2023-09-01 18:33:19 +0530

    Add files via upload

---
+++ assignments/ch1/program3.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Print Fahrenheit-Celsius table
+
+int main(void)
+{
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  printf("Fahr\tCelsius\n");
+  printf("---------------\n");
+
+  fahr = lower;
+  while (fahr <= upper)
+  {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f\t\t%6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+
+  return 0;
+}
