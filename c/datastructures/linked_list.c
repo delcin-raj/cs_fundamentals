65b0a94f418708f84bbba20fb938a73229e882b3
delcin-raj <delcinraj@gmail.com>
2023-11-05 14:54:37 +0530

    Updated info on linked list

--- c/datastructures/linked_list.c
+++ c/datastructures/linked_list.c
@@ -1,20 +1,27 @@
 #include<stddef.h>
 // Recursive data structure
-typedef struct IntLinkedList {
+typedef struct Node {
     int val;
-    struct InLinkedList * next;
-} list;
+    struct Node * next;
+} Node;
+
+typedef struct List {
+    Node *head;
+} List;
 
 // Let's understand the shortcomings of c programming language
 // How to use this structure?
 // Create a linked list with only one member ex., [1]
 
-void printLinkedList(list l) {
+void printLinkedList(List l) {
     // TODO: implement this
 }
 
 int main() {
-    list l = {1, NULL};
+    List empty = {NULL};
+
+    Node one = {1, NULL};
+    List singleton = {&one};
     // We will use a sentinel value to specify the end of the linked list
     // How to print the linked list?
     return 0;
